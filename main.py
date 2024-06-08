845550151c260335bd22facf55f2e17bd726d969
3lackC4t <C.S.Minty@proton.me>
2024-06-08 00:50:25 +0000

    added employee csv file and imported modules into main.py

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 #!/bin/python
 
+import csv
+import sys
+import personnel
 
+
+
+if __name__ == "__main__":
+    new_suzy = personnel.Employee('Suzy','Masterson','Admin', 67000)
+    print(new_suzy.get_salary())
