82e4b7e6b150aa1cd69218e78b1a234dbcb3ebbc
3lackC4t <C.S.Minty@proton.me>
2024-06-08 00:36:13 +0000

    Added User and Contractor classes

--- personnel.py
+++ personnel.py
@@ -1,19 +1,51 @@
 #!/bin/python
 
 class Employee:
-    def __init__(self, fname, lname, title, salary):
+    def __init__(self, fname: str, lname: str, title: str, salary: float):
         self.fname = fname
         self.lname = lname
         self.title = title
         self.salary = salary
 
-    def get_name(self):
+    def get_name(self) -> str:
+        return f"{self.fname} {self.lname}"
 
+    def get_title(self) -> str:
+        return self.title
+
+    def get_salary(self) -> float:
+        return self.salary
+
+    def set_title(self, title) -> None:
+        self.title = title
+
+    def set_salary(self, salary) -> None:
+        self.salary = salary
 
 class User(Employee):
-    def __init__(self, fname, lname, title, salary, admin_status):
-        pass
+    def __init__(self, fname, lname, title, salary, admin_status: bool):
+        super().__init__(fname, lname, title, salary)
+        self.admin_status = admin_status
+
+    def is_admin(self):
+        return self.admin_status
+
+    def set_admin(self):
+        self.admin_status = True
+
+    def revoke_admin(self):
+        self.admin_status = False
 
 class Contractor(Employee):
-    def __init__(self, fname, lname, title, salary, contract_length):
-        pass
+    def __init__(self, fname, lname, title, salary, contract_length: int):
+        super().__init__(fname, lname, title, salary)
+        self.contract_length = contract_length
+
+    def get_contract_length(self) -> int:
+        return self.contract_length
+
+    def set_contract_length(self, length) -> None:
+        self.contract_length = length
+
+    def get_contract_cost(self) -> float:
+        return self.contract_length * self.salary
